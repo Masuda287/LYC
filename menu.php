8e24b026925153b0cce4afe33ae6cb0f6c0b414d
Kaito0712 <148739905+Kaito0712@users.noreply.github.com>
2023-10-31 10:11:03 +0900

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,26 @@
+<input type="checkbox" id="menu-toggle" class="menu-checkbox">
+<label for="menu-toggle" class="menu-button"><span></span></label>
+<header class="drawer-menu">
+  <ul>
+    <li><a href="#">マイページ</a></li>
+    <li><a href="#">ライブラリ</a></li>
+    <li><a href="#">Book</a></li>
+    <li><a href="#">Movie</a></li>
+    <li><a href="#">Music</a></li>
+    <li><a href="#">閲覧履歴</a></li>
+  </ul>
+</header>
+<label for="menu-toggle" class="menu-background"></label>
+
+<dev class="tabs has-background-info-light is-large">
+                    <p></p>
+                    <a href="" class="has-text-centered">Inhome</a>
+                    <?php
+                    $ss=0;
+                    if($ss==0){
+                    echo '<a href="" class="is-size-6 has-text-link">ログイン</a>'; 
+                    }else{
+                    echo '<a href="">ログアウト</a>'; 
+                    }
+                    ?>
+</dev>
