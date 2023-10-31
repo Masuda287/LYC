7464c03ae548289599a126074514a02b180f831d
Masuda287 <148735159+Masuda287@users.noreply.github.com>
2023-10-31 10:23:46 +0900

    ファイル移動削除

--- header.php
+++ header.php
@@ -4,5 +4,5 @@
 <meta charset="UTF-8">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
-<link rel="stylesheet" type="text/css" href="css/header.css?xyz">
+<link rel="stylesheet" type="text/css" href="css/header.css">
 <body>
