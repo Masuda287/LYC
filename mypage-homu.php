f21a6a4c2094f73e20698a35241787dd9a5f4d3d
nishizako <148830243+nishizako@users.noreply.github.com>
2023-11-02 10:29:42 +0900

    f

---
+++ mypage-homu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/mypage-homu.css">
+</head>
+<body>
+<?php
+    echo '<p><button type="submit"><a href="yu-za-jyouhou.php">ユーザー情報</a></button></p>';
+    echo '<p><button type="submit"><a href="yu-za-jyouhou.php">クレジット登録</a></button></p>';
+    echo '<p><button type="submit"><a href="yu-za-jyouhou.php">閲覧履歴</a></button></p>';
+    echo '<p><button type="submit"><a href="yu-za-jyouhou.php">ライブラリ</a></button></p>';
+    echo '<p><button type="submit"><a href="yu-za-jyouhou.php">ユーザー情報削除申請</a></button></p>';
+?>
+</body>
+</html>
