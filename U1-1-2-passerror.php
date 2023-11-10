11dcc500798727cef7bb1d706161f0a1fcb730e3
mothi122 <148734933+mothi122@users.noreply.github.com>
2023-11-10 12:02:56 +0900

    1

---
+++ U1-1-2-passerror.php
@@ -0,0 +1,47 @@
+<?php session_start(); ?>
+<?php require 'db-connect.php'; ?>
+<?php require 'header.php'; ?>
+<?php require 'menu.php'; ?>
+
+<?php
+$u_name=$login=$b_day=$mail=$password=$pass_c='';
+if(isset($_SESSION['customer'])) {
+    $u_name=$_SESSION['customer']['u_name'];
+    $login=$_SESSION['customer']['login'];
+    $b_day=$_SESSION['customer']['b_day'];
+    $mail=$_SESSION['customer']['mail'];
+    $password=$_SESSION['customer']['password'];
+    $pass_c=$_SESSION['customer']['pass_c'];
+}
+?>
+<h1>新規会員登録</h1>
+<?php
+echo  '<form action="U1-1-2-term.php" method="post">';
+echo  '<table>';
+echo  '<tr><td>姓名</td><td>';
+echo  '<input type="text" required name="u_name" value="', $u_name, '">';
+echo  '</td></tr>';
+echo  '<tr><td>ユーザー名</td><td>';
+echo  '<input type="text" required name="login" value="', $login, '">';
+echo  '</td></tr>';
+echo  '<tr><td>生年月日</td><td>';
+echo  '<input type="number"  max="20301231" required name="b_day" value="', $b_day, '">';
+echo  '</td></tr>';
+echo  '<tr><td>メールアドレス</td><td>';
+echo  '<input type="text" required name="mail" value="', $mail, '">';
+echo  '</td></tr>';
+echo  '<tr><td>パスワード</td><td>';
+echo  '<input type="password" required name="password" value="', $password, '">';
+echo  '</td></tr>';
+echo  '<tr><td>パスワード(確認)</td><td>';
+echo  '<input type="password" required name="pass_c" value="', $pass_c, '">';
+echo  '</td></tr>';
+echo  '</table>';
+?>
+<p>パスワードが異なります</p>
+<?php
+echo  '<input type="submit" value="新規登録">';
+echo  '</form>';
+?>
+
+<?php require 'footer.php'; ?>
