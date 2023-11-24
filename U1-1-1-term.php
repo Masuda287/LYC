b2239a63d56e9c3567b570e70a6640cab1c37626
mothi122 <148734933+mothi122@users.noreply.github.com>
2023-11-24 11:28:27 +0900

    Update U1-1-1-term.php

--- U1-1-1-term.php
+++ U1-1-1-term.php
@@ -3,19 +3,29 @@
 <?php 
 
 unset($_SESSION['customer']);
-$pdo=new PDO($connect,user,pass);
+unset($_SESSION['Credit']);
+$pdo=new PDO($connect,USER,PASS);
 $sql=$pdo->prepare('select * from Customer where u_name=?');
 $sql->execute([$_POST['u_name']]);
 foreach($sql as $row) {
     if($_POST['password']==$row['password']){
 
-    
     $_SESSION['customer']=[
         'c_id'=>$row['c_id'], 'u_name'=>$row['c_name'],
         'login'=>$row['u_name'],'b_day'=>$row['birthday'],
          'mail'=>$row['address'],'password'=>$row['password']];
-    }
-}
+
+         $sql=$pdo->prepare('select * from Credit where c_id=?');
+         $sql->execute([$_SESSION['customer']['c_id']]);
+         foreach($sql as $ro)
+         $_SESSION['Credit']=[
+            'c_id'=>$ro['c_id'], 'card_num'=>$ro['card_num'],
+            'card_name'=>$ro['card_name'],'dl_m'=>$ro['dl_m'],
+             'dl_y'=>$ro['dl_y'],'code'=>$ro['code']];
+    } 
+}  
+         
+
 if(isset($_SESSION['customer'])){
          header("Location:U1.php"); 
         
