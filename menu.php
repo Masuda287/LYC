21a0ad28e5cd453ac465c06b8849531e67e35290
mothi122 <148734933+mothi122@users.noreply.github.com>
2023-11-24 11:24:32 +0900

    lo

--- menu.php
+++ menu.php
@@ -1,24 +1,9 @@
-<label for="menu-toggle" class="menu-background"></label>
-
-<dev class="tabs has-background-info-light is-large">
-  
-                    <p></p>
-                    <a href="menu.php" class="has-text-centered">Inhome</a>
-                    <?php
-                    $ss=0;
-                    if($ss==0){
-                    echo '<a href="" class="is-size-6 has-text-link">ログイン</a>'; 
-                    }else{
-                    echo '<a href="logout.php">ログアウト</a>'; 
-                    }
-                    ?>
-</dev>
-<hr>
+<dav>
 <input type="checkbox" id="menu-toggle" class="menu-checkbox">
 <label for="menu-toggle" class="menu-button"><span></span></label>
 <header class="drawer-menu">
   <ul>
-    <li><a href="mypage-homu.php">マイページ</a></li>
+    <li><a href="#">マイページ</a></li>
     <li><a href="#">ライブラリ</a></li>
     <li><a href="#">Book</a></li>
     <li><a href="#">Movie</a></li>
@@ -26,3 +11,17 @@
     <li><a href="#">閲覧履歴</a></li>
   </ul>
 </header>
+<label for="menu-toggle" class="menu-background"></label>
+<dev class="tabs has-background-info-light is-large">
+                    <p class="has-text-info-light">___________</p>
+                    <a href="" class="has-text-centered">Inhome</a>
+                    <?php
+                    $ss=0;
+                    if($ss==0){
+                    echo '<a href="U1-1-1.php" class="is-size-6 has-text-link">ログイン</a>'; 
+                    }else{
+                    echo '<a href="logout.php" class="is-size-6 has-text-link">ログアウト</a>'; 
+                    }
+                    ?>
+</dev>
+</dav>
