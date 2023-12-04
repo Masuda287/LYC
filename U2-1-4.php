ca922cc883bd0543b1cfb2ef88a89b875b60d77d
Naoto200311 <148735731+Naoto200311@users.noreply.github.com>
2023-12-04 14:45:16 +0900

    Update U2-1-4.php

--- U2-1-4.php
+++ U2-1-4.php
@@ -1,21 +1,11 @@
-<?php session_start();?>
 <?php require 'header.php';?>
 <?php require 'menu.php';?>
-<?php require 'db-connect.php';?>
-<?php
-$id = $_SESSION['customer']['c_id'];
-$pdo=new PDO($connect, USER, PASS);
-$sql=$pdo->prepare('update Customer set c_name=?,u_name=?,birthday=?,address=?,password=? where c_id=?');
-            $sql->execute([
-                $_SESSION['u_kosin']['u_name'],$_SESSION['u_kosin']['login'],$_SESSION['u_kosin']['birthday'],$_SESSION['u_kosin']['mail'],$_SESSION['u_kosin']['password'],$id
-            ]);
-            $_SESSION['customer']=[
-                'id'=>$id, 'u_name'=>$_SESSION['u_kosin']['u_name'],
-                'login'=>$_SESSION['u_kosin']['login'],
-                'mail'=>$_SESSION['u_kosin']['mail'],
-                'b_day'=>$_SESSION['u_kosin']['birthday'],
-                'password'=>$_SESSION['u_kosin']['password']];
-            
-                unset($_SESSION['u_kosin']);
-            ?>
-<h1>更新完了</h1>
+<div class="has-text-centered is-size-3">
+更新完了
+</div>
+<div class="has-text-centered m-6">
+ユーザー情報を更新しました
+</div>
+<div class="level-item">
+<a href="U2.php" class="button is-info">戻る</a>
+</div>
