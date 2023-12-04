40916a4b592c6fbc54288199c1474919da74bd61
Naoto200311 <148735731+Naoto200311@users.noreply.github.com>
2023-12-04 15:00:31 +0900

    Update U1-3-4.php

--- U1-3-4.php
+++ U1-3-4.php
@@ -1,16 +1,17 @@
-<?php session_start();?>
 <?php require 'db-connect.php';?>
 <?php require 'header.php'; ?>
 <?php require 'menu.php'; ?>
 
-
-<h1>購入完了</h1>
-<p>ご購入ありがとうございます。</p>
+<div class="has-text-centered">
+<h1 class="is-size-3 pb-3">購入完了</h1>
+<div class="pt-6">ご購入ありがとうございます。</div>
+<div class="pt-6">
+<a href="U1.php" class="button is-info ">トップへ</a>
+</div>
 
 
 
 <?php
-  $pdo=new PDO($connect,USER,PASS);
 $sql=$pdo->prepare('insert into Purchase values(null,?,now())');
 $sql->execute([$_SESSION['customer']['c_id']]);
 $i=$pdo->lastInsertId();
@@ -20,9 +21,9 @@ foreach($sql as $row){
     $sql=$pdo->prepare('insert into Purchase_detail values(?,?)');
         $sql->execute([$i,$row['s_id']]);
 }
-$sql=$pdo->prepare('delete from Cart where c_id=?');
-$sql->execute([$_SESSION['customer']['c_id']]);
+
 ?>
+</div>
 <?php require 'footer.php'; ?>
 
 
