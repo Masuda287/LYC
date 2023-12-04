b1c7fe6a812882ee7e0f96863a5e817aec9e63c9
Naoto200311 <148735731+Naoto200311@users.noreply.github.com>
2023-12-04 14:45:06 +0900

    Update U2-1-2-term.php

--- U2-1-2-term.php
+++ U2-1-2-term.php
@@ -3,38 +3,30 @@
 <?php
 $pdo=new PDO($connect, USER, PASS);
 
-$_SESSION['u_kosin']=[
-    'u_name'=>$_POST['c_name'],
-    'login'=>$_POST['u_name'],'mail'=>$_POST['address'],
-    'birthday'=>$_POST['birthday'],'password'=>$_POST['password']];
+$id=$_SESSION['Customer']['c_id'];
+    $sql=$pdo->prepare('select * from Customer where c_id!=? and u_name=?');
+    $sql->execute([$id,$_POST['u_name']]);
 
-$id=$_SESSION['customer']['c_id'];
-    $sql=$pdo->prepare('select u_name from Customer where c_id!=?');
-    $sql->execute([$id]);
-foreach($sql as $error){
-
-    if($error['u_name']==$_POST['u_name']){
-        header("Location:U2-1-3-name.php"); 
-    }
-}
- 
+    if(empty($sql->fetchAll())){
         if(empty($_POST['c_name'])){
-            header("Location:U2-1-2-input.php");
-        }else if(empty($_POST['u_name'])){
-            header("Location:U2-1-2-input.php");
-        }else if(empty($_POST['birthday'])){
-            header("Location:U2-1-2-input.php");
-        }else if(empty($_POST['address'])){
-            header("Location:U2-1-2-input.php");
-        }else if(empty($_POST['password'])){
-            header("Location:U2-1-2-input.php");
-        }else if(empty($_POST['password1'])){
-            header("Location:U2-1-2-input.php");
-        }else if (!preg_match('/[0-9]+/', $_POST['birthday'])){
-            header("Location:U2-1-2-birth.php"); 
-        }else if($_POST['password']!=$_POST['password1']){ 
-            header("Location:U2-1-2-pass.php"); 
-        }else{
-            header("Location:U2-1-3.php"); 
+            header("Location:U2-1-2error.php");
+        }
+        else if(!preg_match('/[0-9]+/', $_POST['price']))
+        else{
+            $sql=$pdo->prepare('update Customer set c_name=?,u_name=?,birthday=?,address=?,password=? where c_id=?');
+            $sql->execute([
+                $_POST['c_name'],$_POST['u_name'],$_POST['birthday'],$_POST['address'],$_POST['password'],$id
+            ]);
+            $_SESSION['Customer']=[
+                'id'=>$id, 'c_name'=>$_POST['c_name'],
+                'u_name'=>$_POST['u_name'],
+                'address'=>$_POST['address'],
+                'birthday'=>$_POST['birthday'],
+                'password'=>$_POST['password'] ];
+                header("Location:U2-1-3.php"); 
+            }
+        }
+        else{
+            header("Location:U2-1-2error.php"); 
         }
 ?>
