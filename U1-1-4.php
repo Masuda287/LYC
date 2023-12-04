559da124df0cb4e4c9242367f8196a26b7551689
Naoto200311 <148735731+Naoto200311@users.noreply.github.com>
2023-12-04 14:29:21 +0900

    Update U1-1-4.php

--- U1-1-4.php
+++ U1-1-4.php
@@ -1,21 +1,18 @@
-<?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
 <?php require 'menu.php'; ?>
-
-    <?php
-        $pdo=new PDO($connect,USER,PASS);
-        $sql=$pdo->prepare("insert into Customer values(null,?,?,?,?,?,'')");
-        $sql->execute([
-            $_SESSION['kyaku']['u_name'], $_SESSION['kyaku']['login'],$_SESSION['kyaku']['b_day'],$_SESSION['kyaku']['mail'],$_SESSION['kyaku']['password']
-        ]);
-        unset($_SESSION['toroku']);
-    ?>
-<form action="U1-1-1-term.php" method="post">
+<div class="has-text-centered">
+<h1 class="is-size-3">登録完了</h1>
+<form action="U1-1-4-login.php" method="post">
     <p>会員登録が完了しました。</p>
-    <p>ユーザー名とパスワードを入力してログインしてください。</p>
-    ユーザー名<input type="text" name="u_name" class=" is-vcentered"><br>
-    パスワード<input type="password" name="password" class=" is-vcentered"><br>
-    <input type="submit" value="ログイン" >
+    <p class="pb-2">ユーザー名とパスワードを入力してログインしてください。</p>
+    <div class="pb-2">ユーザー名<input type="text" name="u_name" class=" is-vcentered"></div>
+
+    <div class="pb-2">パスワード<input type="password" name="password" class=" is-vcentered"></div>
+    
+    <div class="pt-3">
+    <a href="U1.php" class="button is-info">ログイン</a>
+    </div>
 </form>
+</div>
 <?php require 'footer.php'; ?>
