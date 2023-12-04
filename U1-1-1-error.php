7bfd41cef0d8e7dcc61ed308b987ccca3452cd94
Naoto200311 <148735731+Naoto200311@users.noreply.github.com>
2023-12-04 14:28:44 +0900

    Update U1-1-1-error.php

--- U1-1-1-error.php
+++ U1-1-1-error.php
@@ -1,16 +1,21 @@
         <?php require 'header.php'; ?>
         <?php require 'menu.php'; ?>
-<h1>会員のお客様</h1>
-    <p>ユーザー名とパスワードを入力してください</p>
-    <form action="U1-1-1-term.php" method="post">
-    ユーザー名<input type="text" name="u_name" class=" is-vcentered"><br>
-    パスワード<input type="password" name="password" class=" is-vcentered"><br>
-    <input type="submit" value="ログイン" >
-        <p>ユーザー名もしくはパスワードが違います</p>
-    <h1>初めてのご利用のお客様。会員以外のお客様</h1>
-    <p>初めてのご利用のお客様は、こちらから会員登録を行ってください。</p>
-    <button onclick="location.href='U1-1-2.php' "type="button">新規会員登録</button>
+<form action="U1-1-1-term.php" method="post">
+    <div class="has-text-centered">
+    <h1  class="mt-5 is-size-4">ログイン</h1>
+    <h1  class="is-size-6">会員のお客様</h1>
+    <div class="is-size-7">ユーザーIDとパスワードを入力してください</div><br>
+    <div class="pb-2 is-size-7">ユーザーID<input type="text" name="u_name" class=" is-vcentered"></div>
+    <div class="is-size-7">パスワード<input type="password" name="password" class=" is-vcentered"></div><br>
+    <a href="U1-1-1-term.php" class="button is-info">ログイン</a><br><br>
+    <p class="has-text-danger">ユーザー名もしくはパスワードが違います</p>
 </form>
+    <br>
+    <br>
+    <h1 class="is-size-5">初めてのご利用のお客様・会員以外のお客様</h1>
+    <p class="is-size-7">初めてのご利用のお客様は、こちらから会員登録を行ってください。</p><br>
+    <a href="U1-1-2.php" class="button is-info">新規会員登録</a>
+    </div>
 <?php require 'footer.php'; ?>
     
 
