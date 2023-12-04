b92549d4352d994e923303b97364574c3d8ea17f
Naoto200311 <148735731+Naoto200311@users.noreply.github.com>
2023-12-04 14:29:15 +0900

    Update U1-1-3.php

--- U1-1-3.php
+++ U1-1-3.php
@@ -1,20 +1,32 @@
-<?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
 <?php require 'menu.php'; ?>
+<div class="has-text-centered">
+<div level>
 <?php
-echo  '<form action="U1-1-4.php" method="post">';
-echo  '<h1>入力情報確認</h1>';
-echo  '<p>姓名：',$_SESSION['kyaku']['u_name'],'</p>';
-echo  '<p>ユーザー名：',$_SESSION['kyaku']['login'],'</p>';
-echo  '<p>生年月日：',$_SESSION['kyaku']['b_day'],'</p>';
-echo  '<p>メールアドレス：',$_SESSION['kyaku']['mail'],'</p>';
-echo  '<p>パスワード：', substr_replace($_SESSION['kyaku']['password'],'*',1,5),'</p>';   
-echo  '<p><input type="submit" value="登録"></p>';
-echo  '</form>';
-?>
-
+        $name1=$_GET['u_name'];
+        $login1=$_GET['login'];
+        $birth1=$_GET['b_day'];
+        $mail1=$_GET['mail'];
+        $password1=$_GET['password'];
+        $URL="U1-1-4.php?u_name={$name1}&login={$login1}&b_day={$birth1}&mail={$mail1}&password={$password1}"; 
+        echo  '<form action="',$URL,'" method="post">';
+        echo  '<h1 class="is-size-3">入力情報確認</h1><br>';
+        echo  '<table class="level-item">';
+        echo  '<div class="U1-1-3 has-text-centered">';
+        echo  '<p class="pb-2">姓名：',$_GET['u_name'],'</p>';
+        echo  '<p class="pb-2">ユーザー名：',$_GET['login'],'</p>';
+        echo  '<p class="pb-2">生年月日：',$_GET['b_day'],'</p>';
+        echo  '<p class="pb-2">メールアドレス：',$_GET['mail'],'</p>';
+        echo  '<p class="pb-2">パスワード：',preg_replace('/^A-Za-z0-9/','*',$_GET['password']),'</p>';   
+        echo  '</div>';
+        echo  '</table><br>';
+        echo '<a href="',$URL,'" class="button is-info ">新規登録</a>';
+        echo  '</form>';
+    ?>
+</div>
+</div>
 <?php require 'footer.php'; ?>
 <?php
-// $_GET['password'] echo preg_replace('/0-9{1}/', '*', $GET['password']);
+// $_GET['password'] echo preg_replace('/^A-Za-z0-9{1}/', '*', $GET['password']);
 ?>
