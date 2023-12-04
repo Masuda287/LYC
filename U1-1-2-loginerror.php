66ae95d6ac979c11f108f17c5fc53165859bad2d
Naoto200311 <148735731+Naoto200311@users.noreply.github.com>
2023-12-04 14:29:00 +0900

    Update U1-1-2-loginerror.php

--- U1-1-2-loginerror.php
+++ U1-1-2-loginerror.php
@@ -3,35 +3,47 @@
 <?php require 'header.php'; ?>
 <?php require 'menu.php'; ?>
 <?php
-
+$u_name=$login=$b_day=$mail=$password=$pass_c='';
+if(isset($_SESSION['customer'])) {
+    $u_name=$_SESSION['customer']['u_name'];
+    $login=$_SESSION['customer']['login'];
+    $b_day=$_SESSION['customer']['b_day'];
+    $mail=$_SESSION['customer']['mail'];
+    $password=$_SESSION['customer']['password'];
+    $pass_c=$_SESSION['customer']['pass_c'];
+}
 ?>
-<h1>新規会員登録</h1>
-<p>既に同じログイン名のユーザーが存在するため、</p>
-<p>使用できません</p>
+<div class="has-text-centered">
+<div level>
+    <h1 class="is-size-3">新規会員登録</h1>
 <?php
+echo  '<div class="has-text-danger">既に同じログイン名のユーザーが存在するため、使用できません</div>';
 echo  '<form action="U1-1-2-term.php" method="post">';
-echo  '<table>';
-echo  '<tr><td>姓名</td><td>';
-echo  '<input type="text"   maxlength="50"  name="u_name" value="',$_SESSION['kyaku']['u_name'], '">';
+echo  '<table class="level-item">';
+echo  '<tr><td class="pb-3">姓名</td><td>';
+echo  '<input type="text" required  name="u_name" value="', $u_name, '">';
 echo  '</td></tr>';
-echo  '<tr><td>ユーザー名</td><td>';
-echo  '<input type="text" maxlength="50"   name="login" value="', $_SESSION['kyaku']['login'], '">';
+echo  '<tr><td class="pb-3">ユーザー名</td><td>';
+echo  '<input type="text" required name="login" value="', $login, '">';
 echo  '</td></tr>';
-echo  '<tr><td>生年月日</td><td>';
-echo  '<input type="text"   maxlength="8" name="b_day" value="',$_SESSION['kyaku']['b_day'], '">';
+echo  '<tr><td class="pb-3">生年月日</td><td>';
+echo  '<input type="number" max="20301231" required name="b_day" value="', $b_day, '">';
 echo  '</td></tr>';
-echo  '<tr><td>メールアドレス</td><td>';
-echo  '<input type="email" maxlength="50" name="mail" value="',$_SESSION['kyaku']['mail'], '">';
+echo  '<tr><td class="pb-3">メールアドレス</td><td>';
+echo  '<input type="text" required name="mail" value="', $mail, '">';
 echo  '</td></tr>';
-echo  '<tr><td>パスワード</td><td>';
-echo  '<input type="password"  maxlength="20" name="password" value="',$_SESSION['kyaku']['password'], '">';
+echo  '<tr><td class="pb-3">パスワード</td><td>';
+echo  '<input type="password" required name="password" value="', $password, '">';
 echo  '</td></tr>';
-echo  '<tr><td>パスワード(確認)</td><td>';
-echo  '<input type="password" maxlength="20" name="pass_c" >';
+echo  '<tr><td class="pb-3">パスワード(確認)</td><td>';
+echo  '<input type="password" required name="pass_c" value="', $pass_c, '">';
 echo  '</td></tr>';
 echo  '</table>';
-echo  '<input type="submit" value="新規登録">';
+echo  '<br>';
+echo  '<div class="pb-3"></div>';
+echo  '<a href="U2-1-2-term.php" class="button is-info ">新規登録</a>';
 echo  '</form>';
 ?>
+</div>
 
 <?php require 'footer.php'; ?>
