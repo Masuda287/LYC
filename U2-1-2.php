0fd2a38c4d91aa5d2eef120fc6e6097f9ca52660
Naoto200311 <148735731+Naoto200311@users.noreply.github.com>
2023-12-04 14:45:10 +0900

    Update U2-1-2.php

--- U2-1-2.php
+++ U2-1-2.php
@@ -4,36 +4,38 @@
 <hr>
 <?php 
 $c_name=$u_name=$birthday=$address=$password='';
-if(isset($_SESSION['customer'])){
-    $c_name=$_SESSION['customer']['u_name'];
-    $u_name=$_SESSION['customer']['login'];
-    $birthday=$_SESSION['customer']['b_day'];
-    $address=$_SESSION['customer']['mail'];
-    $password=$_SESSION['customer']['password'];
+if(isset($_SESSION['Customer'])){
+    $c_name=$_SESSION['Customer']['c_name'];
+    $u_name=$_SESSION['Customer']['u_name'];
+    $birthday=$_SESSION['Customer']['birthday'];
+    $address=$_SESSION['Customer']['address'];
+    $password=$_SESSION['Customer']['password'];
 }
-echo '<h1>会員情報更新</h1>';
+echo '<div class="has-text-centered"><div item>';
+echo '<h1 class="is-size-3">会員情報更新</h1><br>';
 echo '<form action="U2-1-2-term.php"method="post">';
-echo '<table>';
-echo '<tr><td>姓名</td><td>';
-echo '<input type="text" maxlength="50"  name="c_name" value="',$c_name,'">';
+echo '<table class="level-item">';
+echo '<tr><td class="pb-3">姓</td><td>';
+echo '<input type="text" name="c_name" value="',$c_name,'">';
 echo '</td></tr>';
-echo '<tr><td>ユーザー</td><td>';
-echo '<input type="text" maxlength="50" name="u_name" value="',$u_name,'">','';
+echo '<tr><td class="pb-3">ユーザー</td><td>';
+echo '<input type="text" name="u_name" value="',$u_name,'">','';
 echo '</td></tr>';
-echo '<tr><td>生年月日</td><td>';
-echo '<input type="number" maxlength="8" name="birthday" value="',$birthday,'">';
+echo '<tr><td class="pb-3">生年月日</td><td>';
+echo '<input type="text" name="birthday" value="',$birthday,'">';
 echo '</td></tr>';
-echo '<tr><td>メールアドレス</td><td>';
-echo '<input type="email" maxlength="50" name="address" value="',$address,'">';
+echo '<tr><td class="pb-3">メールアドレス</td><td>';
+echo '<input type="text" name="address" value="',$address,'">';
 echo '</td></tr>';
-echo '<tr><td>パスワード</td><td>';
-echo '<input type="password" maxlength="20" name="password" value="',$password,'">';
+echo '<tr><td class="pb-3">パスワード</td><td>';
+echo '<input type="password" name="password" value="',$password,'">';
 echo '</td></tr>';
-echo '<tr><td>パスワード確認</td><td>';
-echo '<input type="password" maxlength="20" name="password1" value="">';
+echo '<tr><td class="pb-3">パスワード確認</td><td>';
+echo '<input type="password" name="password1" value="">';
 echo '</td></tr>';
-echo '</table>';
-echo '<a href="U2-1-1.php"><button type="button">戻る</button></a><input type="submit" value="確認">';
+echo '</table><br>';
+echo '<a href="U2-1-2-term.php" class="button is-info ">確認</a>';
 echo '</form>';
+echo '</div>'
 ?>
 <?php require 'footer.php';?>
