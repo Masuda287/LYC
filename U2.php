342959f1360ac496fd104d9207981c38a7a9d93e
Naoto200311 <148735731+Naoto200311@users.noreply.github.com>
2023-12-04 14:46:02 +0900

    Update U2.php

--- U2.php
+++ U2.php
@@ -1,20 +1,18 @@
-<?php session_start();?>
 <?php require 'header.php';?>
 <?php require 'menu.php'?>
 <hr>
-<hr>
 <?php
-if(isset($_SESSION['customer'])){
-    echo '<p><button type="submit"><a href="U2-1-1.php">ユーザー情報</a></button></p>';
-    echo '<p><button type="submit"><a href="U2-term.php">クレジット登録</a></button></p>';
-    echo '<p><button type="submit"><a href="U2-3.php">閲覧履歴</a></button></p>';
-    echo '<p><button type="submit"><a href="U2-4.php">ライブラリ</a></button></p>';
-    echo '<p><button type="submit"><a href="U2-5-1.php">ユーザー情報削除申請</a></button></p>';
-
+    if(isset($_SESSION['Customer'])){
+    echo '<div class="has-text-centered">';
+    echo '<a href="U2-1-1.php" class="button is-large is-info"><p class="has-text-info">///////////</p>ユーザー情報<p class="has-text-info">///////////</p></a><br><br>';
+    echo '<a href="U2-1-1.php" class="button is-large is-info"><p class="has-text-info">//////////</p>クレジット登録<p class="has-text-info">//////////</p></a><br><br>';
+    echo '<a href="U2-1-1.php" class="button is-large is-info"><p class="has-text-info">//////////////</p>閲覧履歴<p class="has-text-info">//////////////</p></a><br><br>';
+    echo '<a href="U2-1-1.php" class="button is-large is-info"><p class="has-text-info">/////////////</p>ライブラリ<p class="has-text-info">/////////////</p></a><br><br>';
+    echo '<a href="U2-1-1.php" class="button is-large is-info"><p class="has-text-info">///////</p>ユーザー情報削除申請<p class="has-text-info">///////</p></a>';
+    echo '</div>';
 }
 else{
     echo 'ログインしてください。';
-}
-
+}   
 ?>
 <?php require 'footer.php';?>
