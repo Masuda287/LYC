2921a9dc9116063f5d419414721ee1f72e2287cf
Naoto200311 <148735731+Naoto200311@users.noreply.github.com>
2023-12-04 14:45:12 +0900

    Update U2-1-3.php

--- U2-1-3.php
+++ U2-1-3.php
@@ -2,17 +2,20 @@
 <?php require 'header.php';?>
 <?php require 'menu.php';?>
 <hr>
+<div class="has-text-centered">
+<div level>
 <?php
-
-
-    echo '<h1>更新確認</h1>';
-    echo '<table>';
-    echo '<tr><td>姓名：</td><td>', $_SESSION['u_kosin']['u_name'],'</td></tr>';
-    echo '<tr><td>ユーザー名:</td><td>', $_SESSION['u_kosin']['login'],'</td></tr>';
-    echo '<tr><td>生年月日:</td><td>', $_SESSION['u_kosin']['birthday'],'</td></tr>';
-    echo '<tr><td>メールアドレス:</td><td>', $_SESSION['u_kosin']['mail'],'</td></tr>';
-    echo '<tr><td>パスーワード:</td><td>', $_SESSION['u_kosin']['password'],'</td></tr>';
+    echo '<h1 class="is-size-3">更新確認</h1>';
+    echo '<table class="level-item">';
+    echo '<tr><td class="pb-3">姓名：</td><td>', $_SESSION['Customer']['c_name'],'</td></tr>';
+    echo '<tr><td class="pb-3">ユーザー名:</td><td>', $_SESSION['Customer']['u_name'],'</td></tr>';
+    echo '<tr><td class="pb-3">生年月日:</td><td>', $_SESSION['Customer']['birthday'],'</td></tr>';
+    echo '<tr><td class="pb-3">メールアドレス:</td><td>', $_SESSION['Customer']['address'],'</td></tr>';
+    echo '<tr><td class="pb-3">パスワード:</td><td>', $_SESSION['Customer']['password'],'</td></tr>';
     echo '</table>';
-    echo '<p><button type="submit"><a href="U2-1-2.php">戻る</a></button><button type="submit"><a href="U2-1-4.php">更新</a></button></p>'
+    echo '<a href="U2-1-2.php" class="button is-medium">戻る</a>';
+    echo '<a class="has-text-white">______________</a>';
+    echo '<a href="U2-1-3.php" class="button is-info is-medium">更新</a>';
 ?>
+</div>
 <?php require 'footer.php';?>
